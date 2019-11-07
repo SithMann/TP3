f925311062df68103b64cca9a9c82cdcb8c3cdf5
Despres Mathis <s171162@ic2s114-07.univ-lemans.fr>
2019-11-07 13:08:21 +0100

    hop hop hop

--- tp3.c
+++ tp3.c
@@ -1,52 +1,67 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/time.h>
+#include <sys/wait.h>
 
 int main( int argc, char * argv[]){
+    int tube[2], cr, no_err;
+    pipe(tube);
+    struct timeval tdebv, tfinv;
+    float tempsmoyfils, tempsmoy = 0, temp;
 
     if(argc != 4){
         fprintf(stderr,"Mauvais nombre d'arguments \n");
+        fprintf(stderr,"TP3.c : <nb de ps fils> <nb d'exec> <commande> \n");
         exit(1);
     }
-
     int n = atoi(argv[1]);
     int k = atoi(argv[2]);
-    char * c = argv[3];
+    char c[strlen(argv[3])];
+    strcpy(c, argv[3]);
 
     for(int i = 0; i < n; i++){
-        switch(fork()){
-            case -1:fprintf(stderr,"Erreur sur le fork\n");
-                    exit(1);
-            case 0: /* Le processus est un fils */
-                    if(!gettimeofday(&tdeb, )){
-                        fprintf(stderr,"Problème récupération du temps\n");
-                        exit(2);
-                    }
-                    for(int j = 0; j < k; j++){
-                        switch(fork()){
-                            case -1:fprintf(stderr,"Erreur sur le fork\n");
-                                    exit(3);
-                            case 0: /* Le processus est un petit fils */
-                                    switch(fork()){
-                                        case -1:fprintf(stderr,"Erreur sur le fork\n");
-                                            exit(4);
-                                        case 0: execl(c,c,NULL);
-                                        default:
-                                    }
-                            default: /* Le processus est un fils */
-                        }
-                    }
-                    while(wait(&cr) == -1);
-                    if(!gettimeofday(&tfin, )){
-                        fprintf(stderr,"Problème récupération du temps\n");
-                        exit(5);
-                    }
-            default: /* Le processus est le père */
+        if((no_err = fork()) == 0){
+         /* Le processus est un fils */
+            if(gettimeofday(&tdebv, NULL)){
+                fprintf(stderr,"Problème récupération du temps\n");
+                exit(2);
+            }
+            for(int j = 0; j < k; j++){
+                if((no_err = fork())  == 0){
+                    /* Le processus est un petit fils */
+                    if(execlp(c,c,NULL) == -1)
+                       fprintf(stderr,"Probleme sur le execl\n");
+                          
+                }else if(no_err == -1){
+                    fprintf(stderr,"Erreur sur le fork n°2\n");
+                    exit(3);
+                }
+            }
+            while(wait(&cr) != -1);
+            if(gettimeofday(&tfinv, NULL)){
+                fprintf(stderr,"Problème récupération du temps\n");
+                exit(4);
+            }
+            tempsmoyfils = (float)(( tfinv.tv_sec * 1000000 + tfinv.tv_usec ) - ( tdebv.tv_sec * 1000000 + tdebv.tv_usec ))/k;
+            write(tube[1], &tempsmoyfils, sizeof(float));
+            exit(0);
+        }else if(no_err == -1){
+            fprintf(stderr,"Erreur sur le fork n°1\n");
+            exit(5);
         }
     }
-
+    while(wait(&cr) != -1);
+    close(tube[1]);
+    while(read(tube[0], &temp, sizeof(float))){
+        tempsmoy += temp;
+    }
+    close(tube[0]);
+    tempsmoy /= n;
+    tempsmoy /= 1000000;
+    printf("Temps Moyen d'execution de %s = %.3f \n", c, tempsmoy);
+    exit(0);
 }
-
-// prendre le temps avant l'exec des k commandes
-// diviser par le nombre de commandes à la fin
-// faire un exec.h comme Mathieu c'est smart
