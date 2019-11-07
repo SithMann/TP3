d8eeeacd8283c7a5e27be7632a87162e12ad1d4b
Despres Mathis <s171162@ic2s112-01.univ-lemans.fr>
2019-11-07 16:19:19 +0100

    terminé

--- tp3.c
+++ tp3.c
@@ -20,10 +20,10 @@ int main( int argc, char * argv[]){
     char c[strlen(argv[3])];
     strcpy(c, argv[3]);
 
-    int tube[2], cr, no_err, i = 0, pid,x;
+    int tube[2], cr, no_err, i = 0, j, pid,x;
     pipe(tube);
     struct timeval tdebv, tfinv;
-    float tempsmoyfils, tempsmoy = 0, temp, lesTemps[n];
+    float tempsmoy = 0, temp, lesTemps[n], tempsfils;
 
     for(int i = 0; i < n; i++){
         if((no_err = fork()) == 0){
@@ -44,26 +44,25 @@ int main( int argc, char * argv[]){
                     fprintf(stderr,"Erreur sur le fork n°2\n");
                     exit(3);
                 }
-            }
-            while((pid = wait(&cr)) != -1);
-
-            /* En cas de terminaison anormale */
-            if(cr != 0){
-                /*Interruption par signal, les bits de poids faibles contiennent le numero, les autres sont à 0 */
-                if(cr>>8 == 0)
-                    fprintf(stderr, "Fin du processus de la commande interrompu par un signal, numero de signal = %d\n ",cr);
-                /* exit du fils != 0, code retour du fils dans les 8 bits de poids forts, les autres sont à 0*/
-                else
-                    fprintf(stderr, "Fin du processus de la commande avec un cr != 0, numero de cr = %d\n", cr>>8);
-                exit(5);
-            }else{
-                if(gettimeofday(&tfinv, NULL)){
-                    fprintf(stderr,"Problème récupération du temps\n");
-                    exit(4);
+                while((pid = wait(&cr)) != -1);
+                /* En cas de terminaison anormale */
+                if(cr != 0){
+                    /*Interruption par signal, les bits de poids faibles contiennent le numero, les autres sont à 0 */
+                    if(cr>>8 == 0)
+                        fprintf(stderr, "Fin du processus de la commande interrompu par un signal, numero de signal = %d\n ",cr);
+                    /* exit du fils != 0, code retour du fils dans les 8 bits de poids forts, les autres sont à 0*/
+                    else
+                        fprintf(stderr, "Fin du processus de la commande avec un cr != 0, numero de cr = %d\n", cr>>8);
+                    exit(5);
+                }else{
+                    if(gettimeofday(&tfinv, NULL)){
+                        fprintf(stderr,"Problème récupération du temps\n");
+                        exit(4);
+                    }
+                    tempsfils = (float)(( tfinv.tv_sec * 1000000 + tfinv.tv_usec ) - ( tdebv.tv_sec * 1000000 + tdebv.tv_usec ));
+                    write(tube[1], &tempsfils, sizeof(float));
+                    exit(0);
                 }
-                tempsmoyfils = (float)(( tfinv.tv_sec * 1000000 + tfinv.tv_usec ) - ( tdebv.tv_sec * 1000000 + tdebv.tv_usec ))/k;
-                write(tube[1], &tempsmoyfils, sizeof(float));
-                exit(0);
             }
         }else if(no_err == -1){
             fprintf(stderr,"Erreur sur le fork n°1\n");
@@ -94,11 +93,11 @@ int main( int argc, char * argv[]){
 
         printf("\tN° Processus i\t|\tMi (en sec)\n");
         printf("-------------------------------------------\n");
-        for(i = 0; i < n; i++){
-            printf("\t%d\t\t|\t%.4f \n", i, lesTemps[i]);
+        for(j = 0; j < n; j++){
+            printf("\t%d\t\t|\t%.4f \n", j, lesTemps[j]);
         }
 
-        printf("\nLe résultat final est le milieu du tableau, tab[%d] = %.4f sec\n\n", n/2, lesTemps[n/2]);
+        printf("\nLe résultat final est le milieu du tableau, tab[%d] = %.4f sec\n\n", i/2, lesTemps[i/2]);
         exit(0);
     }
 }
